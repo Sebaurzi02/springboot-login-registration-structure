f56ab4457288bceb0ada10b3584e3c80ed96f160
Sebastiano <RZUSST02B24C351W@studium.unict.it>
2023-04-20 21:39:16 +0200

    start

---
+++ src/main/java/com/pp/springbootwebapp2/SpringBootWebapp2Application.java
@@ -0,0 +1,13 @@
+package com.pp.springbootwebapp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebapp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebapp2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pp/springbootwebapp2/SpringBootWebapp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pp.springbootwebapp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebapp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
