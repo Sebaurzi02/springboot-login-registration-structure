bf78a7505237ed321f3967d32c7520b61a14bf3a
Sebastiano <RZUSST02B24C351W@studium.unict.it>
2023-04-21 09:07:10 +0200

    Login html page + css + Fix java code
    exclude -> default login page spring security , fix Controller for login (Front-end)

--- src/main/java/com/pp/springbootwebapp2/Controller/HomeController.java
+++ src/main/java/com/pp/springbootwebapp2/Controller/HomeController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
     
-    @GetMapping("/home")
+    @GetMapping("/login")
     public String home(){
-        return "index.html";
+        return "login";
     }
     
 }
