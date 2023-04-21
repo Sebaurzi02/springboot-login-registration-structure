727ab14561851c8fe98139ae38de0f14d3acc6f6
Sebastiano <RZUSST02B24C351W@studium.unict.it>
2023-04-21 08:25:40 +0200

    RuoliRepository fix
    change method variable , name to rname

--- src/main/java/com/pp/springbootwebapp2/Repository/RuoliRepository.java
+++ src/main/java/com/pp/springbootwebapp2/Repository/RuoliRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.pp.springbootwebapp2.model.Ruolo;
 
 public interface RuoliRepository extends JpaRepository<Ruolo,Integer>{
-    Ruolo findByName(String name);
+    Ruolo findByRname(String rname);
 }
