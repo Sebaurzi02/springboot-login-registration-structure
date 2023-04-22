83f02cee94dce1ef951c391166d7090ce9fb5101
Sebastiano <RZUSST02B24C351W@studium.unict.it>
2023-04-22 19:39:15 +0200

    implements salvaUtente method
    use class data transfer for User and method checkRuolo() ;

--- src/main/java/com/pp/springbootwebapp2/services/US_Impl.java
+++ src/main/java/com/pp/springbootwebapp2/services/US_Impl.java
@@ -1,10 +1,14 @@
 package com.pp.springbootwebapp2.services;
 
+import java.util.Arrays;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.pp.springbootwebapp2.Repository.RuoliRepository;
 import com.pp.springbootwebapp2.Repository.UserRepository;
+import com.pp.springbootwebapp2.model.Ruolo;
+import com.pp.springbootwebapp2.model.User;
 import com.pp.springbootwebapp2.transferdata.UserDt;
 
 @Service
@@ -20,10 +24,28 @@ public class US_Impl implements UserService {
         this.passwordEncoder=passwordEncoder;
     }
 
+    private Ruolo cheackRuolo(){
+        Ruolo ruolo = new Ruolo();
+        ruolo.setRname("ROLE_ADMIN");
+        return ruoliRepository.save(ruolo);
+    }
+
     @Override
     public void salvaUtente(UserDt userDt){
-        
+        User utente = new User();
+        utente.setName(userDt.getName());
+        utente.setPassword(passwordEncoder.encode(userDt.getPassword()));
+
+        Ruolo ruolo = ruoliRepository.findByRname("RUOLO_ADMIN");
+        if(ruolo == null){
+            ruolo = cheackRuolo();
+        }
+
+        utente.setRuoli(Arrays.asList(ruolo));
+        userRepository.save(utente);
     }
+
+
 
     
 }
