6dd1150a49d073164fd13e38063d2d037896cc63
Sebastiano <RZUSST02B24C351W@studium.unict.it>
2023-04-25 00:13:34 +0200

    Registrazione & Login HTML file + Controller
    change style   +  Controller signup page html

--- src/main/java/com/pp/springbootwebapp2/Controller/HomeController.java
+++ src/main/java/com/pp/springbootwebapp2/Controller/HomeController.java
@@ -1,7 +1,10 @@
 package com.pp.springbootwebapp2.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.pp.springbootwebapp2.transferdata.UserDt;
 
 @Controller
 public class HomeController {
@@ -9,6 +12,13 @@ public class HomeController {
     @GetMapping("/login")
     public String home(){
         return "login";
+    }
+
+    @GetMapping("/signup")
+    public String signup(Model model){
+        UserDt utente = new UserDt();
+        model.addAttribute("utente",utente);
+        return "registrazione";
     }
     
 }
